ca6b4e297952cc8a00cf0e7fe1603e2f8b76b060
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-17 09:00:53 +0700

    Create the project

---
+++ TwitterProfilePage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterProfilePage
+//
+//  Created by Danh Tu on 17/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterProfilePage/TwitterProfilePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterProfilePageApp.swift
+//  TwitterProfilePage
+//
+//  Created by Danh Tu on 17/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterProfilePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
